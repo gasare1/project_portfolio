30ba112413af63d32e2624fe298cadba2f2b6694
Glen Asare <gasare1@asu.edu>
2021-10-03 08:39:42 -0700

    Update Signup.js

--- src/Components/LoginSignup/Signup.js
+++ src/Components/LoginSignup/Signup.js
@@ -25,7 +25,7 @@ const Register = () => {
   };
   const submit = async (e) => {
     e.preventDefault();
-    await fetch(" https://glenasare15.pythonanywhere.com/portfolioregister", {
+    await fetch("https://glenasare15.pythonanywhere.com/portfolioregister", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
