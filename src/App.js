a4de821092b2bd02e6ef9d491577c4eac3760a91
Glen Asare <gasare1@asu.edu>
2021-10-11 20:42:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navigbar from "./Components/NavBar/Navbar/navigbar";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Components/HomeComponent/Home";
 import Projects from "./Components/Projects/Projects";
 import { scrollToTop } from "react-scroll/modules/mixins/animate-scroll";
