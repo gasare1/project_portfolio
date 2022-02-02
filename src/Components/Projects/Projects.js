08fb20456192036c6b3a984c0d8fdec271785a2b
Glen Asare <gasare1@asu.edu>
2022-02-02 14:07:31 -0700

    fixed bootstrap with mobile version

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -26,90 +26,73 @@ import "./project.css";
 function Projects() {
   AOS.init();
   return (
-    <div id="projects">
-      <ProjHome
-        className="project"
-        style={{
-          justifyContent: "center",
-          display: "flex",
-          margin: "100px",
-          fontFamily: "Comfortaa, cursive",
-        }}
-        data-aos="fade-up"
-        data-aos-duration="3000"
-      >
-        <Container>
-        <Row xs={1} md={2}>
-        <Col  md={4}>
+    <div id="projects"  data-aos="fade-up"
+    data-aos-duration="3000">
+      <Container style={{marginTop:'20px'}}>
+        <Row>
+          <Col xs={12} md={4}>
+            <Card className="shadow-lg p-3 mb-5 bg-white rounded" id="card2">
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title style={{ fontWeight: "bolder" }}>
+                  Call Mr Moe
+                </Card.Title>
+                <Card.Text>
+                  This is a Real Estate Website that was build with ReactJs
+                  Framework and Python Backend
+                </Card.Text>
+                <Button variant="primary" href="https://callmrmoe.com">
+                  Access Page
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={4}>
+            <Card className="shadow-lg p-3 mb-5 bg-white rounded" id="card2">
+              <Card.Img variant="top" src={image4} />
+              <Card.Body>
+                <Card.Title style={{ fontWeight: "bolder" }}>
+                  Admin Console
+                </Card.Title>
+                <Card.Text>
+                  This is a Admin that was build with ReactJs Framework and
+                  Random User API.. *NOT Mobile Responsive yet work is still in
+                  progress
+                </Card.Text>
+                <Button
+                  variant="primary"
+                  href="https://adminconsole.netlify.app"
+                >
+                  Access Page
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} md={4}>
+            <Card className="shadow-lg p-3 mb-5 bg-white rounded" id="card2">
+              <Card.Img variant="top" src={image2} />
+              <Card.Body>
+                <Card.Title style={{ fontWeight: "bolder" }}>
+                  Bliss Records
+                </Card.Title>
+                <Card.Text>
+                  This is a Record Label Website it was built using Reactjs and
+                  Python Backend
+                </Card.Text>
+                <Button variant="primary" href="https://blissrecords.net">
+                  Access Page
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row >
+        <Col xs={12} md={4}>
         <Card
-          style={{ width: "18rem", margin: "50px" }}
+         
           className="shadow-lg p-3 mb-5 bg-white rounded"
-          className="card2"
-        >
-          <Card.Img variant="top" src={image} />
-          <Card.Body>
-            <Card.Title style={{ fontWeight: "bolder" }}>
-              Call Mr Moe
-            </Card.Title>
-            <Card.Text>
-              This is a Real Estate Website that was build with ReactJs
-              Framework and Python Backend
-            </Card.Text>
-            <Button variant="primary" href="https://callmrmoe.com">
-              Access Page
-            </Button>
-          </Card.Body>
-        </Card>
-        </Col>
-        <Col md={4}>
-        <Card
-          style={{ width: "18rem", margin: "50px" }}
-          className="shadow-lg p-3 mb-5 bg-white rounded"
-          className="card2"
-        >
-          <Card.Img variant="top" src={image4} />
-          <Card.Body>
-            <Card.Title style={{ fontWeight: "bolder" }}>
-              Admin Console
-            </Card.Title>
-            <Card.Text>
-              This is a Admin that was build with ReactJs Framework and Random
-              User API.. *NOT Mobile Responsive yet work is still in progress
-            </Card.Text>
-            <Button variant="primary" href="https://adminconsole.netlify.app">
-              Access Page
-            </Button>
-          </Card.Body>
-        </Card>
-        </Col>
-        
-        <Col md={1}>
-        <Card
-          style={{ width: "18rem", margin: "50px" }}
-          className="shadow-lg p-3 mb-5 bg-white rounded"
-          className="card2"
-        >
-          <Card.Img variant="top" src={image2} />
-          <Card.Body>
-            <Card.Title style={{ fontWeight: "bolder" }}>
-              Bliss Records
-            </Card.Title>
-            <Card.Text>
-              This is a Record Label Website it was built using Reactjs and
-              Python Backend
-            </Card.Text>
-            <Button variant="primary" href="https://blissrecords.net">
-              Access Page
-            </Button>
-          </Card.Body>
-        </Card>
-        </Col>
-        <Row xs={1} md={2}>
-        <Col>
-        <Card
-          style={{ width: "18rem", margin: "50px" }}
-          className="shadow-lg p-3 mb-5 bg-white rounded"
-          className="card2"
+          id="card2"
         >
           <Card.Img variant="top" src={image3} />
           <Card.Body>
@@ -127,12 +110,12 @@ function Projects() {
         </Col>
         <Col>
         </Col>
-        </Row>
-        <Col md={4}>
+        
+        <Col xs={12} md={4} >
         <Card
-          style={{ width: "18rem", margin: "50px" }}
+          
           className="shadow-lg p-3 mb-5 bg-white rounded"
-          className="card2"
+          id="card2"
         >
           <Card.Img variant="top" src={image5} />
           <Card.Body>
@@ -148,9 +131,11 @@ function Projects() {
           </Card.Body>
         </Card>
         </Col>
+        <Col>
+        
+        </Col>
         </Row>
         </Container>
-      </ProjHome>
     </div>
   );
 }
--- src/Components/Projects/Projele.js
+++ src/Components/Projects/Projele.js
@@ -2,9 +2,5 @@ import styled from 'styled-components'
 
 export const ProjHome = styled.div`
 flex-direction:row;
-    @media screen and (max-width: 2080px) {
-        height:auto;
-        justify-content:center;
-        display:flex;
-      }
+    
 `
